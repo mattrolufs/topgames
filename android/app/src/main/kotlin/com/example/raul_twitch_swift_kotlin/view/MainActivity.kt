50acf082077ce05c4947272f08b64c678b855d59
mattrolufs <mattrolufs@yahoo.com>
2019-09-21 17:31:19 -0700

    added TopGamesViewModel, fetching successfully

--- android/app/src/main/kotlin/com/example/raul_twitch_swift_kotlin/view/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/raul_twitch_swift_kotlin/view/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.raul_twitch_swift_kotlin.view
 
 import android.content.Intent
 import android.os.Bundle
+import com.example.raul_twitch_swift_kotlin.model.NativeTwitchDataSource
+import com.example.raul_twitch_swift_kotlin.viewmodel.TopGamesModel
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity: FlutterActivity() {
 
@@ -14,6 +19,8 @@ class MainActivity: FlutterActivity() {
   companion object {
     const val CHANNEL = "dataChannel"
   }
+
+
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -27,12 +34,12 @@ class MainActivity: FlutterActivity() {
       }
     }
 
-//    var methodChannel = MethodChannel(flutterView, CHANNEL)
-//    methodChannel.setMethodCallHandler { call, result ->
-//                        if(call.method.equals("startNewActivity")) {
-//                          startNewActivity()
-//                        }
-//    }
+    val twitchDataSource = NativeTwitchDataSource()
+
+    GlobalScope.launch(Dispatchers.Main) {
+      val topGamesResponse = twitchDataSource.getTopGames(20).await()
+      System.out.println(topGamesResponse.data[0].gameId)
+    }
 
   }
 
--- android/app/src/main/kotlin/com/example/raul_twitch_swift_kotlin/view/TopGamesActivity.kt
+++ android/app/src/main/kotlin/com/example/raul_twitch_swift_kotlin/view/TopGamesActivity.kt
@@ -2,13 +2,29 @@ package com.example.raul_twitch_swift_kotlin.view
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.example.raul_twitch_swift_kotlin.R
+import com.example.raul_twitch_swift_kotlin.viewmodel.TopGamesModel
 
-class TopGamesActivity: Activity() {
+class TopGamesActivity: FragmentActivity() {
+
+    private lateinit var viewModel: TopGamesModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.top_games)
+
+        viewModel = ViewModelProviders.of(this).get(TopGamesModel::class.java)
+
+        viewModel.fetchTopGames(20).observe(this, Observer {
+
+            System.out.println(it.data[0].title)
+
+        })
+
     }
 
 }
